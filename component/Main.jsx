6dd8136e582e6502e1e9b8b3f50ffc5917e3daa2
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-10 15:37:57 +0200

    Update  App.js

---
+++ component/Main.jsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRoute } from "../router";
+import { authStateChangeUser } from "../redux/auth/authOperations";
+import { getCurrentPosition } from "../helpers/getCurrentPosition";
+
+const Main = () => {
+    const { stateChande } = useSelector((state) => state.auth);
+    const routing = useRoute(stateChande);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(authStateChangeUser());
+        getCurrentPosition(dispatch);
+    }, []);
+
+    return <NavigationContainer>{routing}</NavigationContainer>;
+};
+
+export default Main;
